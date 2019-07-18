1563a8bba9cd483ae5c3b11b7402f0a4b1c78e1f
jake-fox <47289805+jake-fox@users.noreply.github.com>
2019-07-18 20:23:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ echo "sudo systemctl status ts3proxy.service"
 echo "Restart Required"
 echo "do you accept <Y> <N>"
 read
-if [ $reply y ] || [ $reply Y ]
+if [ $reply -eq y ] || [ $reply -eq Y ]
 then 
 reboot
 fi
